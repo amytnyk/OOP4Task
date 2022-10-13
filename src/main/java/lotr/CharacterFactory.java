f66e54e7c806ba6b5558ac31635015f6c5cf1ae3
amytnyk <alex.mytnyk@ukr.net>
2022-10-13 20:21:56 +0300

    added org.reflections

--- src/main/java/lotr/CharacterFactory.java
+++ src/main/java/lotr/CharacterFactory.java
@@ -1,13 +1,16 @@
 package lotr;
 
+import org.reflections.Reflections;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Random;
 
 public class CharacterFactory {
+    static Reflections reflections = new Reflections("org.example.lotr");
     Character createCharacter() {
         try {
-            List<Class<? extends Character>> characters = List.of(Elf.class, Hobbit.class, King.class, Knight.class);
+            List<Class<? extends Character>> characters = reflections.getSubTypesOf(Character.class).stream().toList();
             return characters.get((new Random(characters.size())).nextInt()).getConstructor().newInstance();
         } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
